602fbdb60f0af7e49aed2dd9fa7164a306178253
nanatoto-droid <imanisalome95@gmail.com>
2025-02-05 09:13:32 +0300

    bmicalculation

--- bmicalculation.py
+++ bmicalculation.py
@@ -1,7 +1,17 @@
 # user input
-weight=float(input("Enter your weight: "))
-height=float(input("Enter your height: "))
+weight=float(input("Enter your weight in kg: "))
+height=float(input("Enter your height in meters: "))
 
 #calculation
 bmi=weight/(height**2)
-print("Your BMI is",bmi)
+
+if bmi < 18.5:
+    classification="Underweight"
+elif  bmi < 25:
+    classification="normal weight"
+elif bmi < 30:
+    classification="Overweight"
+elif bmi < 40:
+    classification="Obesity"
+
+print(f"Your BMI is{bmi},which is  considerd {classification}")
