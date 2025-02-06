022c2e07e6f555436a8e080e5da990d93ff3baf2
nanatoto-droid <imanisalome95@gmail.com>
2025-02-06 10:49:28 +0300

    modules

---
+++ function.py
@@ -0,0 +1,11 @@
+def my_function():
+    print("Hello ,this is a function")
+my_function()
+my_function()
+
+def add_numbers():
+    num1=int(input("Enter the first number: "))
+    num2=int(input("Enter the second number: "))
+    print(f"The sum of the two numbers is  {num1+num2}")
+add_numbers()
+
---
+++ function_parameters.py
@@ -0,0 +1,13 @@
+def func_para(name,age,Gender):
+    print(f"Hello my is {name} i am {age} years old and my gender is {Gender}")
+func_para("Imani",18,"Female")
+func_para("Imani",18,"Female")
+func_para("Imani",18,"Female")
+func_para("Imani",18,"Female")
+func_para("Imani",18,"Female")
+
+def sum_of_numbers(a,b):
+    return a+b
+print(sum_of_numbers(10,20))
+sum_of_numbers(10,20)
+
